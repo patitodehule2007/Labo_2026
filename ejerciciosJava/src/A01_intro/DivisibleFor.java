367049b8c48332f1e448f9c3f77f887c6b93e191
patodehule1 <lucasgcastelli@gmail.com>
2026-03-18 09:31:07 -0300

    fix: correccion ej 3

--- ejerciciosJava/src/A01_intro/DivisibleFor.java
+++ ejerciciosJava/src/A01_intro/DivisibleFor.java
@@ -3,10 +3,22 @@ package A01_intro;
 public class DivisibleFor {
     public static void main(String[] args) {
         int i;
-        for(i = 0; i <= 100; i++){
-            if(i % 2 == 0 || i % 3 == 0)
-            System.out.println(i);
+        for(i = 0; i <= 100; i++) {
+            if (i % 6 == 0) {
+                System.out.println(i);
+            }
         }
+
+        i = 0;
+
+        while(i <= 100){
+            if (i % 6 == 0) {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+
 
     }
 }
